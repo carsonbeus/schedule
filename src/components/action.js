ddcd0241dc8e93fe2f957e0cb357bfa3df07332f
Carson Beus <carson.beus@gmail.com>
2018-10-11 09:44:46 -0600

    sytled add remove button

--- src/components/action.js
+++ src/components/action.js
@@ -2,13 +2,38 @@ import React, { Component } from 'react';
 import Icon from './icon';
 
 class Action extends Component {
+
+    handleAction = function() {
+        this.props.onClick()
+
+        // document.getElementById('action').classList.add('')
+    }.bind(this);
+
     render() {
-        return(
-            <button onClick={() => this.props.onClick()} className={`${this.props.className} action`}>
-                { Icon('fas fa-plus-circle') }
-                {/* fas fa-times-circle */}
-            </button>
+        return (
+            <a 
+            id='action'
+            onClick={() => this.props.onClick()} 
+            className={`${this.props.className} action`}>
+              
+            </a>
         )
-  }
+    }
 }
 export default Action;
+
+
+// import React, { Component } from 'react';
+// import Icon from './icon';
+
+// class Action extends Component {
+//     render() {
+//         return(
+//             <button onClick={() => this.props.onClick()} className={`${this.props.className} action`}>
+//                 { Icon('fas fa-plus-circle') }
+//                 {/* fas fa-times-circle */}
+//             </button>
+//         )
+//     }
+// }
+// export default Action;
